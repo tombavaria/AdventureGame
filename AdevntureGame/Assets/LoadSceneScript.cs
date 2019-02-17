2b5ecf8579dc537d1bef6f0b462d944bae672067
tombavaria <35585173+tombavaria@users.noreply.github.com>
2019-02-17 14:17:25 -0500

    Load Levels On Trigger

---
+++ AdevntureGame/Assets/LoadSceneScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadSceneScript : MonoBehaviour
+{
+    public GameObject guiObject;
+    public string levelToLoad;
+
+    void Start ()
+    {
+        guiObject.SetActive(false);
+    }
+
+    void OnTriggerStay (Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            guiObject.SetActive(true); 
+            if (guiObject.activeInHierarchy == true && Input.GetButtonDown("Submit"))
+            {
+                Application.LoadLevel(levelToLoad);
+            }
+        }
+    }
+    void OnTriggerExit()
+    {
+        guiObject.SetActive(false);
+    }
+}
